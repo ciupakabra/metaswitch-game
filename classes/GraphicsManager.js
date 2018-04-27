046fe765b955c23011429fdad4330658b04e91d1
Skylord-Guthix <33182886+Skylord-Guthix@users.noreply.github.com>
2018-04-27 13:41:56 +0100

    Made cables easier to see
    thicc

--- classes/GraphicsManager.js
+++ classes/GraphicsManager.js
@@ -95,7 +95,7 @@ class GraphicsManager {
 
 	spriteInitCable(cable) {
 		cable.graphics = game.make.graphics();
-		cable.graphics.lineStyle(1, 0xffffff);
+		cable.graphics.lineStyle(2, 0xffffff);
 		cable.graphics.moveTo(cable.node_1.x, cable.node_1.y);
 		cable.graphics.lineTo(cable.node_2.x, cable.node_2.y);
 		cable.graphics.endFill();
